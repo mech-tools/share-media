ce995231de632b14d70be6e2b0cc578592cc5000
DarkDinDoN <maxime.chicault@gmail.com>
2023-09-06 01:20:43 +0200

    Using a bundler

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require('gulp');
+const compiler = require('webpack');
+const webpack = require('webpack-stream');
+const named = require('vinyl-named');
 const prefix = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('node-sass'));
@@ -27,9 +30,22 @@ function compileScss() {
         .pipe(prefix({
             cascade: false
         }))
-        .pipe(gulp.dest("./styles"))
+        .pipe(gulp.dest("./build"))
 }
 const css = gulp.series(compileScss);
+
+/* ----------------------------------------- */
+/*  Compile Javascript
+/* ----------------------------------------- */
+
+const SYSTEM_JS = ["modules/**/*.js"];
+function compileJs() {
+    return gulp.src(["modules/main.js", "modules/socket.js"])
+        .pipe(named())
+        .pipe(webpack({ mode: "production"}, compiler))
+        .pipe(gulp.dest('./build'))
+}
+const js = gulp.series(compileJs);
 
 /* ----------------------------------------- */
 /*  Watch Updates
@@ -37,6 +53,7 @@ const css = gulp.series(compileScss);
 
 function watchUpdates() {
     gulp.watch(SYSTEM_SCSS, css);
+    gulp.watch(SYSTEM_JS, js);
 }
 
 /* ----------------------------------------- */
@@ -45,9 +62,12 @@ function watchUpdates() {
 
 exports.default = gulp.series(
     compileScss,
+    compileJs,
     watchUpdates
 );
 exports.build = gulp.series(
-    compileScss
+    compileScss,
+    compileJs
 );
 exports.css = css;
+exports.js = js;
